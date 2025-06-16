10484d4d32563add38d0555d601599a5fa86645e
jvras58 <jvras@cin.ufpe.br>
2025-06-16 02:05:11 -0300

    fix: corrige expressão regular no middleware para incluir o caractere de fim de string

--- TaskSphere/apps/web/middleware.ts
+++ TaskSphere/apps/web/middleware.ts
@@ -11,6 +11,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|sign-in|sign-up|forgot-password|recovery-password).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|sign-in|sign-up|forgot-password|recovery-password|$).*)",
   ],
 };
